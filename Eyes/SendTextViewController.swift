b49a0f867e254eaf58d359c1595547e61bd6aa9d
avacr <c18ac@dalton.org>
2017-07-27 10:02:10 -0400

    pre lecture day #4 - can text from app

--- Eyes/SendTextViewController.swift
+++ Eyes/SendTextViewController.swift
@@ -9,9 +9,11 @@
 import UIKit
 import Contacts
 import ContactsUI
+import MessageUI
 
-class SendTextViewController: UIViewController {
+class SendTextViewController: UIViewController, MFMessageComposeViewControllerDelegate {
     @IBOutlet weak var sendButton: UIButton!
+    @IBOutlet weak var messageTextView: UITextView!
     
     var contact: CNContact?
     
@@ -22,6 +24,13 @@ class SendTextViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     @IBAction func sendButtonTapped(_ sender: UIButton) {
+        
+        let messageViewController = MFMessageComposeViewController()
+        messageViewController.body = messageTextView.text!
+        messageViewController.recipients = [person.namesNumbers[contact!.givenName]!]
+        messageViewController.messageComposeDelegate = self
+        
+        self.present(messageViewController, animated: true, completion: nil)
     }
 
     override func didReceiveMemoryWarning() {
@@ -34,6 +43,11 @@ class SendTextViewController: UIViewController {
         print(contact!.givenName)
         print(person.namesNumbers[contact!.givenName]!)
         //HERE YOU WILL FORMULATE AND SEND THE TEXT USING THE TUTORIAL https://www.youtube.com/watch?v=tDQtAReaRwI
+    }
+    
+    func messageComposeViewController(_ controller: MFMessageComposeViewController, didFinishWith result: MessageComposeResult) {
+        //can put code depending on which result of the message - successful, cancel, failure ...
+        controller.dismiss(animated: true, completion: nil)
     }
     
 
