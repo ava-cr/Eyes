cd723e9ab70b02b706b901026a7a57b3d373d5a2
avacr <c18ac@dalton.org>
2017-08-07 11:55:36 -0400

    started testing with local notifications - can send with button push

--- Eyes/ActionViewController.swift
+++ Eyes/ActionViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 import ContactsUI
 import Contacts
 import MessageUI
+import UserNotifications
 
 
 class ActionViewController: UIViewController, MFMessageComposeViewControllerDelegate {
@@ -20,13 +21,22 @@ class ActionViewController: UIViewController, MFMessageComposeViewControllerDele
     
     var person = Person()
     var contacts = [Contact]()
-
+    var isGrantedNotificationAccess:Bool = false
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         person = CoreDataHelperPerson.retrievePerson()[0]
         contacts = CoreDataHelperContact.retrieveContacts()
+        
+        // local notifications
+        UNUserNotificationCenter.current().requestAuthorization(
+            options: [.alert,.sound,.badge],
+            completionHandler: { (granted,error) in
+                self.isGrantedNotificationAccess = granted
+        }
+        )
         
         //customization
         alertButton.layer.cornerRadius = 8
@@ -43,12 +53,33 @@ class ActionViewController: UIViewController, MFMessageComposeViewControllerDele
         super.viewDidAppear(true)
         alertButton.flash()
     }
+    @IBAction func testNotificationTapped(_ sender: Any) {
+        if isGrantedNotificationAccess{
+            //add notification code here
+            let content = UNMutableNotificationContent()
+            content.title = "Check in with Eyes!"
+            //content.subtitle = "From Eyes"
+            content.body = "It's been half an hour, slide to assure us you're ok."
+            content.categoryIdentifier = "message"
+            let trigger = UNTimeIntervalNotificationTrigger(
+                timeInterval: 5.0,
+                repeats: false)
+            let request = UNNotificationRequest(
+                identifier: "10.second.message",
+                content: content,
+                trigger: trigger
+            )
+            UNUserNotificationCenter.current().add(
+                request, withCompletionHandler: nil)
+        }
+    }
+    
     
     @IBAction func alertButtonTapped(_ sender: Any) {
         print("alertButtonTapped")
         let messageViewController = MFMessageComposeViewController()
         messageViewController.body = "\(String(describing: person.name)) has sent an ALERT, signalling a potentially dangerous situation. Please try to get in touch with \(String(describing: person.name)) immediately. This alert has been sent to every one of \(String(describing: person.name))'s predetermined contacts."
-
+        
         var numbers = [String]()
         for contact in contacts {
             numbers.append(contact.phoneNumber ?? "")
@@ -61,13 +92,13 @@ class ActionViewController: UIViewController, MFMessageComposeViewControllerDele
         
         self.present(messageViewController, animated: true, completion: nil)
     }
-  
+    
     @IBAction func deactivateButtonTapped(_ sender: UIButton) {
         print("deactivateButtonTapped")
     }
     
     @IBAction func unwindToActionView(segue:UIStoryboardSegue) { }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
